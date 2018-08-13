ba62e0911c2f4bf2f035abc1fe1355c700dd0cb5
Kizuree <aldenis1231@hotmail.com>
2018-08-13 14:52:37 -0400

    update

--- styles/js/info.js
+++ styles/js/info.js
@@ -32,7 +32,7 @@ function updateDB(event){
     }
 
     database.ref('users/' + currentUser.uid).set(value);
-    
+    window.location.href = "file:///C:/Users/ASC%20Student/Documents/groupFinder/login.html"
     // then redirect to website.html
 
 
